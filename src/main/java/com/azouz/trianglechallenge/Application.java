2936965da3b96f1d0e0ba678cd58a927c5e3af08
Mohamed Azouz <mohamedaliazouz@gmail.com>
2017-05-17 23:13:50 +0200

    reading inputs from applicaiton arguments, adding tests

--- src/main/java/com/azouz/trianglechallenge/Application.java
+++ src/main/java/com/azouz/trianglechallenge/Application.java
@@ -5,16 +5,46 @@ import com.azouz.trianglechallenge.domain.TriangleType;
 import com.azouz.trianglechallenge.exception.NoSuchTriangleTypeException;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
 
 /**
  * @author mazouz
  */
 public class Application {
 
-    public static void main(final String[] args) throws NoSuchTriangleTypeException {
-        final Triangle triangle = Triangle.builder().
-                withSide1(2).withSide2(3).withSide3(4).build();
+    public static void main(final String[] args) {
+        try {
+            final Application app = new Application();
+            app.startApplication(args);
+        } catch (final RuntimeException ex) {
+            System.err.println(MessageFormat.format("Invalid input: {0}", ex.getMessage()));
+        } catch (final NoSuchTriangleTypeException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void startApplication(final String[] args) throws RuntimeException, NoSuchTriangleTypeException {
+        final Triangle triangle = getTriangleFrom(args);
         final TriangleType typeOptional = TriangleType.fromSides(triangle);
-        System.out.println(MessageFormat.format("{0} -> {3}", triangle, typeOptional.getTypeName()));
+        System.out.println(MessageFormat.format("{0} is {1} Triangle", triangle, typeOptional.getTypeName()));
+    }
+
+
+    public Triangle getTriangleFrom(final String[] args) throws RuntimeException {
+        if (args == null || args.length < 3) {
+            throw new RuntimeException(MessageFormat.format("Please check your input: {0}", Arrays.toString(args)));
+        }
+        return Triangle.builder().
+                withSide1(getSideLengthFrom(args[0])).
+                withSide2(getSideLengthFrom(args[1])).
+                withSide3(getSideLengthFrom(args[2])).build();
+    }
+
+    public int getSideLengthFrom(final String input) throws RuntimeException {
+        final int value = Integer.valueOf(input);
+        if (value <= 0) {
+            throw new RuntimeException(MessageFormat.format("Side length should be > 0: {0}", input));
+        }
+        return value;
     }
 }
--- src/test/java/com/azouz/trianglechallenge/ApplicationTest.java
+++ src/test/java/com/azouz/trianglechallenge/ApplicationTest.java
@@ -1,15 +1,103 @@
 package com.azouz.trianglechallenge;
 
+import com.azouz.trianglechallenge.domain.Triangle;
 import com.azouz.trianglechallenge.exception.NoSuchTriangleTypeException;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author mazouz
  */
 public class ApplicationTest {
+    private Application app;
 
     @Test
-    public void testMain() throws NoSuchTriangleTypeException {
-        Application.main(null);
+    public void getTriangleFromWithValidInput() {
+        final String[] args = new String[]{"1", "2", "3"};
+        final Triangle triangle = app.getTriangleFrom(args);
+        assertEquals(triangle.getSide1(), 1);
+        assertEquals(triangle.getSide2(), 2);
+        assertEquals(triangle.getSide3(), 3);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getTriangleFromWithNullInput() {
+        app.getTriangleFrom(null);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getTriangleFromWithLessInput() {
+        final String[] args = new String[]{"1", "2"};
+        app.getTriangleFrom(args);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getTriangleFromWithInvalidInputLessInput() {
+        final String[] args = new String[]{"1", "2"};
+        app.getTriangleFrom(args);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getInputWithInvalidInputWithInvalidZeroSideLength() {
+        final String[] args = new String[]{"0", "1", "3"};
+        app.getTriangleFrom(args);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getInputWithInvalidInputWithInvalidLessThanZeroSideLength() {
+        final String[] args = new String[]{"1", "1", "-3"};
+        app.getTriangleFrom(args);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getTriangleFromWithInvalidInputWithCharacterSideLength() {
+        final String[] args = new String[]{"aa", "1", "-3"};
+        app.getTriangleFrom(args);
+    }
+
+    @Before
+    public void setup() {
+        app = new Application();
+    }
+
+
+    @Test(expected = RuntimeException.class)
+    public void getSideLengthFromInvalidValueShouldThrowException() {
+        app.getSideLengthFrom("0");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getSideLengthFromInvalidCharacterValueShouldThrowException() {
+        app.getSideLengthFrom("11asdwewe1");
+    }
+
+    @Test
+    public void getSideLengthFromValidInput() {
+        int value = app.getSideLengthFrom("1");
+        assertEquals(value, 1);
+        value = app.getSideLengthFrom("22");
+        assertEquals(value, 22);
+        value = app.getSideLengthFrom("989898");
+        assertEquals(value, 989898);
+    }
+
+    @Test
+    public void startApplicationWithValidInputShouldNotThrowwExceptions() throws NoSuchTriangleTypeException {
+        final String[] args = new String[]{"1", "1", "1"};
+        app.startApplication(args);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void startApplicationWithInValidInputShouldThrowwException() throws NoSuchTriangleTypeException {
+        final String[] args = new String[]{"1", "1", "-1"};
+        app.startApplication(args);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void startApplicationWithInValidInput1ShouldThrowwException() throws NoSuchTriangleTypeException {
+        final String[] args = new String[]{"1asd1", "1", "1"};
+        app.startApplication(args);
     }
 }
